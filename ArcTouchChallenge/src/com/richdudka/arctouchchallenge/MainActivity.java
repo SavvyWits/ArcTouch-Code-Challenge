6620396acaa447112f226435deddfbf0a8e5775c
Rich Dudka <richdudka@gmail.com>
2014-08-24 20:39:58 -0700

    Fixed keyboard visibility

--- ArcTouchChallenge/src/com/richdudka/arctouchchallenge/MainActivity.java
+++ ArcTouchChallenge/src/com/richdudka/arctouchchallenge/MainActivity.java
@@ -1,11 +1,13 @@
 package com.richdudka.arctouchchallenge;
 
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -40,6 +42,11 @@ public class MainActivity extends ListActivity implements MainResultReceiver.Rec
 		intent.putExtra("requestData", data);
 		intent.putExtra("requestType", "list");
 		startService(intent);
+		
+		InputMethodManager inputManager = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+		inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                   InputMethodManager.HIDE_NOT_ALWAYS);
 	}
 
 	@Override
